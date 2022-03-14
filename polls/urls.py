aba0878f86354be01853f48b6812ca6d73d2d648
Vikas Verma <vikas0713@gmail.com>
2022-03-14 15:12:45 +0530

    Django static files

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from polls.views import polls, rate, create_poll
+from polls.views import polls, rate, create_poll, poll_details
 
 urlpatterns = [
     path('poll-list/', polls),
     path('poll-create/', create_poll, name='create_poll'),
+    path('poll-details/<int:poll_id>/', poll_details, name='poll_details'),
     path('rate/<int:poll_id>/', rate, name='rate'),
 ]
