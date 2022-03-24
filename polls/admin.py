1c7ae2767105f88241ef3089e3d6439468d3c85b
Vikas Verma <vikas0713@gmail.com>
2022-03-24 15:19:10 +0530

    Added model admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from polls.models import Poll
+
+
+class PollAdmin(ModelAdmin):
+
+    list_display = ('id', 'title', 'description')
+    ordering = ('title', )
+    search_fields = ('title', )
+
+
+admin.site.register(Poll, PollAdmin)
